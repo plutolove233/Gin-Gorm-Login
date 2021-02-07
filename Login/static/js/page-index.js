e730ffdd07851873383e4e905cb35b1e2562a0d9
刘浩宇 <71169941+plutolove233@users.noreply.github.com>
2021-02-07 19:32:00 +0800

    v3

--- Login/static/js/page-index.js
+++ Login/static/js/page-index.js
@@ -1,11 +1,13 @@
 window.onload = function(){
-	var ph = document.getElementById("phone").value;
+	var ph = document.getElementById("phone");
 	var phoneReg = /^1[3-9][0-9]{9}$/;
 	var btn = document.getElementById("su");
 	btn.onclick = function(){
-		if(!(phoneReg.test(ph))){
+		if(phoneReg.test(ph.value)){
+			alert("正确");
+		}
+		else{
 			alert("手机号输入有误");
-			return false;
 		}
 		
 	}
